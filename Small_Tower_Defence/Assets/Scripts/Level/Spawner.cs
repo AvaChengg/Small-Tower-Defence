739733b87b6f0733db0842d29d53f611a4ac4aee
AvaCheng <61714486+AvaChengg@users.noreply.github.com>
2023-02-15 23:11:23 +0800

    add spawner

--- Small_Tower_Defence/Assets/Scripts/Level/Spawner.cs
+++ Small_Tower_Defence/Assets/Scripts/Level/Spawner.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour
+public class Spawner : LevelController
 {
-    [Header("Spawner")]
-    [SerializeField] private GameObject _prefab;
+    private void Start()
+    {
+        GetPatrolPoints();
+    }
+    private void GetPatrolPoints()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform patrolPoint = transform.GetChild(i);
+            _patrolPoints.Add(patrolPoint);
+        }
+    }
 }
