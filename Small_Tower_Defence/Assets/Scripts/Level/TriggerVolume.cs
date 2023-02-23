5c8b5a7737ce0b1f58d3041ec6521b77e339ff41
AvaCheng <61714486+AvaChengg@users.noreply.github.com>
2023-02-23 21:22:34 +0800

    Add terminal point system

--- Small_Tower_Defence/Assets/Scripts/Level/TriggerVolume.cs
+++ Small_Tower_Defence/Assets/Scripts/Level/TriggerVolume.cs
@@ -1,20 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class TriggerVolume : MonoBehaviour
 {
-    [SerializeField] private string _tagFilter = "Enemy";
+    [SerializeField] private int _lives = 30;                    // player lives
+    [SerializeField] private WavesEncounter _wavesEncounter;
 
+    public bool IsGameOver;
     public UnityEvent<GameObject> OnEnter;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!string.IsNullOrEmpty(_tagFilter) && !other.CompareTag(_tagFilter)) return;
+        if (!other.TryGetComponent(out EnemyController enemy)) return;
+
+        // deactivated and remove the monster in list
+        _wavesEncounter.RemoveEnemy(enemy);
+        enemy.gameObject.SetActive(false);
+
+        CheckLives();
 
         // kill monsters to start next level
         OnEnter.Invoke(other.gameObject);
     }
 
+    private void CheckLives()
+    {
+        // remove enemy from the list
+        if (_lives > 0) _lives--;
+
+        // check if it's game over
+        if (_lives == 0)
+        {
+            IsGameOver = true;
+
+            // show game over UI
+        }
+    }
 }
