20d950e641009a7c4bff3c4eecb92a13ac6d9c18
AvaCheng <61714486+AvaChengg@users.noreply.github.com>
2023-03-01 00:15:34 +0800

    change camera moving (moving by mouse))

--- Small_Tower_Defence/Assets/Scripts/Camera/CameraController.cs
+++ Small_Tower_Defence/Assets/Scripts/Camera/CameraController.cs
@@ -1,6 +1,4 @@
 using Cinemachine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,17 +8,14 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float _lookOffset = 1.0f;
     [SerializeField] private float _cameraAngle = 45.0f;
     [SerializeField] private float _defaultZoom = 5.0f;
-    [SerializeField] private float _targetSpeed = 8.0f;
     [SerializeField] private LayerMask _groundMask;
 
-    private Vector3 _moveInput;
     private Vector3 _cameraPosition;
     private PlayerController _playerController;
     private CameraMovement _cameraMovement;
     private CinemachineVirtualCamera _levelCamera;
 
     [HideInInspector] public LayerMask GroundMask => _groundMask;
-    [HideInInspector] public bool IsMouseInput;
     [HideInInspector] public Vector3 MoveTarget;
     [HideInInspector] public Vector3 MousePos;
 
@@ -42,16 +37,6 @@ public class CameraController : MonoBehaviour
         _levelCamera.transform.position = _cameraPosition;
     }
 
-    // receive move input from PlayerInput component (Vector2)
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        // check if it's input by keyboard or mouse
-        IsMouseInput = false;
-
-        // read Vector2 data from InputValue
-        Vector2 value = context.ReadValue<Vector2>();
-        _moveInput = new Vector3(value.x, 0, value.y);
-    }
 
     // place the building
     public void OnPlace(InputAction.CallbackContext context)
@@ -60,18 +45,41 @@ public class CameraController : MonoBehaviour
         _playerController.PlaceBuliding();
     }
 
-    private void FixedUpdate()
-    {
-        MoveTarget += (transform.forward * _moveInput.z + transform.right * _moveInput.x) * Time.fixedDeltaTime * _targetSpeed;
-    }
-
     private void Update()
     {
         if (_cameraMovement == null) return;
 
         // send move input to CameraMovement component
-        _cameraMovement.SetMoveInput(IsMouseInput);
-        //_cameraMovement.SetMouseMoveInput(IsMouseInput);
+        _cameraMovement.SetMouseMoveInput();
 
     }
 }
+
+
+
+
+
+
+
+    //[SerializeField] private float _targetSpeed = 8.0f;
+    //private Vector3 _moveInput;
+    //[HideInInspector] public bool IsMouseInput;
+
+    //// receive move input from PlayerInput component (Vector2)
+    //public void OnMove(InputAction.CallbackContext context)
+    //{
+    //    // check if it's input by keyboard or mouse
+    //    IsMouseInput = false;
+
+    //    // read Vector2 data from InputValue
+    //    Vector2 value = context.ReadValue<Vector2>();
+    //    _moveInput = new Vector3(value.x, 0, value.y);
+    //}
+
+    //private void FixedUpdate()
+    //{
+    //    MoveTarget += (transform.forward * _moveInput.z + transform.right * _moveInput.x) * Time.fixedDeltaTime * _targetSpeed;
+    //}
+
+        //// in update()
+        //_cameraMovement.SetMoveInput(IsMouseInput);
--- Small_Tower_Defence/Assets/Scripts/Camera/CameraMovement.cs
+++ Small_Tower_Defence/Assets/Scripts/Camera/CameraMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraMovement : MonoBehaviour
 {
@@ -8,43 +9,43 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] private float _moveSpeed = 4.0f;
     [SerializeField] private float _panBorderThickness = 10.0f;
 
-    private Vector3 _moveInput;
-    private CameraController _cameraController;
-
-    private void Start()
+    public void SetMouseMoveInput()
     {
-        _cameraController = GetComponent<CameraController>();
-    }
-
-    public void SetMoveInput(bool isMouseInput)
-    {
-        if (isMouseInput) return;
-
-        transform.position = Vector3.Lerp(transform.position, _cameraController.MoveTarget, _moveSpeed * Time.deltaTime);
-    }
-
-    public void SetMouseMoveInput(bool isMouseInput)
-    {
-        if (_cameraController.MousePos.y >= Screen.height - _panBorderThickness)
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        if (mousePos.y >= Screen.height - _panBorderThickness)
         {
-            _cameraController.IsMouseInput = true;
+            //_cameraController.IsMouseInput = true;
             transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime, Space.World);
         }
-        if (_cameraController.MousePos.y <= _panBorderThickness)
+        if (mousePos.y <= _panBorderThickness)
         {
-            _cameraController.IsMouseInput = true;
+            //_cameraController.IsMouseInput = true;
             transform.Translate(Vector3.back * _moveSpeed * Time.deltaTime, Space.World);
         }
-        if (_cameraController.MousePos.x >= Screen.width - _panBorderThickness)
+        if (mousePos.x >= Screen.width - _panBorderThickness)
         {
-            _cameraController.IsMouseInput = true;
+            //_cameraController.IsMouseInput = true;
             transform.Translate(Vector3.right * _moveSpeed * Time.deltaTime, Space.World);
         }
-        if (_cameraController.MousePos.x <= _panBorderThickness)
+        if (mousePos.x <= _panBorderThickness)
         {
-            _cameraController.IsMouseInput = true;
+            //_cameraController.IsMouseInput = true;
             transform.Translate(Vector3.left * _moveSpeed * Time.deltaTime, Space.World);
         }
     }
 
+    //private Vector3 _moveInput;
+    //private CameraController _cameraController;
+
+    //private void Start()
+    //{
+    //    _cameraController = GetComponent<CameraController>();
+    //}
+
+    //public void SetMoveInput(bool isMouseInput)
+    //{
+    //    if (isMouseInput) return;
+
+    //    transform.position = Vector3.Lerp(transform.position, _cameraController.MoveTarget, _moveSpeed * Time.deltaTime);
+    //}
 }
