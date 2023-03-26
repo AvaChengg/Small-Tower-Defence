fc943bc6d1fbb026ac802f85b796a213223dc6bc
Erin Beardy <gd65erin@vfs.com>
2023-03-26 15:16:14 -0700

    Added pause icon and scripted functionality for the menu

--- Small_Tower_Defence/Assets/Scripts/PauseMenu.cs
+++ Small_Tower_Defence/Assets/Scripts/PauseMenu.cs
@@ -5,10 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    [SerializeField] GameObject pauseMenu;
+
     public string MainMenu;
 
     public GameObject settingsScreen;
     public GameObject pauseScreen;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
 
     public void ReturnToMenu()
     {
